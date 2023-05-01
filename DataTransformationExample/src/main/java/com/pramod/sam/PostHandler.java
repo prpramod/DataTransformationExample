cb17924bfc2675784c3727108a8186dcde834fe0
prpramod <pramod.luffy@gmail.com>
2023-05-01 21:51:53 +0530

    get the request map and create the reponse map object in Lambda

--- DataTransformationExample/src/main/java/com/pramod/sam/PostHandler.java
+++ DataTransformationExample/src/main/java/com/pramod/sam/PostHandler.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -20,7 +22,22 @@ public class PostHandler implements RequestHandler<Map<String,String>, Map<Strin
 
     public Map<String,String> handleRequest(final Map<String,String> input, final Context context) {
 
+        String firstName = input.get("firstName");
+        String lastName = input.get("lastName");
+        String email = input.get("email");
+        String password = input.get("password");
+        String repeatPassword = input.get("repeatPassword");
+        LambdaLogger logger = context.getLogger();
+        logger.log("\n firstName = " + firstName);
+        logger.log("\n lastName = " + lastName);
+
         Map<String,String> response = new HashMap<String,String>();
+        response.put("id", UUID.randomUUID().toString());
+        response.put("firstName", firstName);
+        response.put("lastName",lastName);
+        response.put("email", email);
+
+
         return response;
 
     }
